3336818855ffe3020c602c7c05beff74101da611
Mindaugas Šunokas <46446902+Mindasteris@users.noreply.github.com>
2022-09-14 12:09:51 +0300

    Add files via upload

---
+++ assets/footer.php
@@ -0,0 +1,24 @@
+<div class=".footer">
+    <footer>
+        <div class="c1">
+            <h3>NEWS</h3>
+        </div>
+        <div class="c2">
+            <h3>GET IN TOUCH</h3>
+            <form action="#">
+            <label for="news"></label>
+            <input type="text" placeholder="Enter Your E-mail" required>
+            <div><button class="btn" value="SUBSCRIBE">SEND</button></div>
+            </form>
+        </div>
+        <div class="c3">
+            <h3>FIND US</h3>
+                <div class="social-icons">
+                    <a href="#"><i class="fa-brands fa-facebook"></i></a>
+                    <a href="#"><i class="fa-brands fa-twitter"></i></a>
+                    <a href="#"><i class="fa-brands fa-youtube"></i></a>
+                </div>
+        </div>
+    </footer>
+    <div class="copyrights"><span>© All Rights Reserved. 2022</span></div>
+</div>
---
+++ assets/nav.php
@@ -0,0 +1,12 @@
+<div>
+    <nav>
+        <ul class="main-menu">
+            <a href="home.php"><img src="./img/Audi-Logo.png" alt="Logo" style="width: 100px;"></a>
+            <li><a href="home.php">Home</a></li>
+            <li><a href="models.php">Models</a></li>
+            <li><a href="news.php">News</a></li>
+            <li><a href="about.php">About</a></li>
+            <li><a href="contact.php">Contact</a></li>
+        </ul>
+    </nav>
+</div>
